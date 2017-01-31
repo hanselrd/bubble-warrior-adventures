4d8772694906cc93f688cf8f5d58b598ec0977da
hanselrd <hansel809@hotmail.com>
2017-01-30 22:15:34 -0500

    Cleaned up configuration file

--- bubble_warrior_adventures/config.lua
+++ bubble_warrior_adventures/config.lua
@@ -1,15 +1,15 @@
-config = {
-	-- Sets the game to fullscreen if true, windowed if false
-	-- using the values defined under resolution
-	fullscreen = false,
-	resolution = {
-		-- Sets the x component of game's resolution
-		x = 800,
-		-- Sets the y component of game's resolution
-		y = 600
-	},
-	-- Defines where to find game scripts
-	script_dir = 'scripts/',
-	-- Defines where to find game assets
-	assets_dir = 'assets/'
+-- Sets the game to fullscreen if true, windowed if false
+-- using the values defined under resolution
+fullscreen = false
+resolution = {
+	-- Sets the x component of game's resolution
+	x = 800,
+	-- Sets the y component of game's resolution
+	y = 600
 }
+-- Where to find game scripts
+script_dir = 'scripts/'
+-- Look at _assets_dir
+assets_dir = 'assets/'
+-- Where to find fonts
+fonts_dir = assets_dir .. 'fonts/'
--- bubble_warrior_adventures/src/game.cpp
+++ bubble_warrior_adventures/src/game.cpp
@@ -1,21 +1,22 @@
 #include "game.hpp"
+#include <iostream>
 #include <utility>
 
 constexpr const char* WINDOW_TITLE = "Bubble Warrior Adventures!";
 
 bwa::Game::Game() {
 	_lua.script_file("config.lua");
-	sol::table config = _lua["config"];
-	sol::table resolution = config["resolution"];
+	sol::table resolution = _lua["resolution"];
 	auto xy = std::make_pair(
 		resolution["x"].get<unsigned>(), 
 		resolution["y"].get<unsigned>());
-	if (config["fullscreen"].get<bool>())
+	if (_lua["fullscreen"].get<bool>())
 		_window.create({ xy.first, xy.second }, WINDOW_TITLE,
 			sf::Style::Fullscreen);
 	else
 		_window.create({ xy.first, xy.second }, WINDOW_TITLE);
 	_window.setVerticalSyncEnabled(true);
+	std::cout << _lua["fonts_dir"].get<std::string>() << std::endl;
 }
 
 void bwa::Game::run() {
