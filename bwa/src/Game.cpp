bd16669e8b8982cdd628522db1169a86687b7f8f
hanselrd <hansel809@hotmail.com>
2017-02-02 22:39:38 -0500

    Changed from game to Game

---
+++ bwa/src/Game.cpp
@@ -0,0 +1,82 @@
+#include "Game.hpp" // relative header, check CONTRIBUTING.md
+#include <exception>
+#include <string>
+#include <utility>
+#include "ResourceLoader.hpp"
+
+constexpr const char* WINDOW_TITLE = "Bubble Warrior Adventures!";
+
+bwa::Game::Game() {
+	// Loads the config into the sol::state
+	_lua.script_file("config.lua");
+
+	// Converts the config's x and y resolution into a table
+	sol::table resolution = _lua["resolution"];
+
+	ResourceLoader<sf::Music>::get("bacon"); // compile error, why?
+
+	// Gets font from resource loader and binds it to _text
+	auto font = ResourceLoader<sf::Font>::get(_lua["game_fonts"]["normal"]);
+	_text.setFont(*font);
+	_text.setFillColor(sf::Color::Yellow);
+	_text.setString("FPS:");
+
+	// Pull the x and y window resolution coordinates from the config
+	auto xy = std::make_pair(
+		resolution["x"].get<unsigned>(), 
+		resolution["y"].get<unsigned>());
+
+	// Create a fullscreen window if set to true in config, windowed if false.
+	if (_lua["fullscreen"].get<bool>())
+		_window.create({ xy.first, xy.second }, 
+			WINDOW_TITLE,
+			sf::Style::Fullscreen);
+	else
+		_window.create({ xy.first, xy.second }, WINDOW_TITLE);
+
+	// Lock FPS to monitor's refresh rate and binds _window to _gui
+	_window.setVerticalSyncEnabled(true);
+	_gui.setWindow(_window);
+}
+
+void bwa::Game::run() {
+	// Create the clock and set it up for the FPS counter
+	sf::Clock clock, update_fps;
+	float last_time = 0.f, current_time, fps;
+	bool show_fps_counter = _lua["show_fps_counter"];
+
+	// Test code to render a green circle
+	sf::CircleShape box;
+	box.setRadius(16.f);
+	box.setFillColor(sf::Color::Green);
+	box.setPosition({ 100.f, 100.f });
+
+	// Normal window event loop
+	while (_window.isOpen()) {
+		sf::Event e;
+		while (_window.pollEvent(e)) {
+			if (e.type == sf::Event::Closed ||
+				sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				_window.close();
+			_gui.handleEvent(e);
+		}
+
+		// Calculates fps
+		if (show_fps_counter) {
+			current_time = clock.restart().asSeconds();
+			fps = 1.f / (current_time - last_time);
+			if (update_fps.getElapsedTime() > sf::seconds(1.f)) {
+				_text.setString("FPS: " + std::to_string(unsigned(fps)));
+				update_fps.restart();
+			}
+		}
+		_window.clear();
+		_window.draw(box);
+		_gui.draw();
+
+		// Displays FPS if show_fps_counter is true
+		if (show_fps_counter)
+			_window.draw(_text);
+		_window.display();
+	}
+}
