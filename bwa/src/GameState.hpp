ee7627ddf92e2308035e60a57490066d18172afa
hanselrd <hansel809@hotmail.com>
2017-02-03 23:12:38 -0500

    Fixed typo in GameState

--- bwa/src/GameState.hpp
+++ bwa/src/GameState.hpp
@@ -1,10 +1,10 @@
 #pragma once
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 namespace bwa {
 	class GameState {
 	public:
-		virtual void draw(sf::Window& window) = 0;
+		virtual void draw(sf::RenderWindow& window) = 0;
 		virtual void handleEvents(sf::Event& e) = 0;
 		virtual void update(float delta) = 0;
 	};
