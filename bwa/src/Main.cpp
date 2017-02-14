b2b7e2512c5fb2a316f1b3ca22b8530ecaa8242c
hanselrd <hansel809@hotmail.com>
2017-02-13 23:49:21 -0500

    Add files via upload

---
+++ bwa/src/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdexcept>
+#include "Game.hpp"
+
+int main(int argc, char** argv) {
+	try {
+		bwa::Game g;
+		g.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}
