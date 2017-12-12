73e2b3053e4a467ef7d8ea8a512f47420bf3a58d
Hansel <hansel809@hotmail.com>
2017-12-12 17:45:48 -0500

    Fixed double class name on constructors

--- src/NPC.hpp
+++ src/NPC.hpp
@@ -5,7 +5,7 @@ class NPC final: public Entity {
 public:
     NPC();
     NPC(sf::IntRect);
-    NPC::NPC(Map::Object mapObject);
+    NPC(Map::Object mapObject);
     NPC(std::string filePath, std::string enemyName, unsigned spriteFormat);
 
     void update(float delta);
